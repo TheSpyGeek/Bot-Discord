b4e7c5a1767ac25713ccb170f78d9e2e81e52c75
thespygeek <isnel.maxime@gmail.com>
2019-11-05 14:34:59 +0100

    le bot marche

--- bot.js
+++ bot.js
@@ -1,14 +1,8 @@
 
-var Discord = require('discord.js');
-//var logger = require('winston');
+const Discord = require('discord.js');
 var auth = require('./auth.json');
-// Configure logger settings
-/*logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});*/
 
-//logger.level = 'debug';
+
 // Initialize Discord Bot
 var bot = new Discord.Client({
    token: auth.token,
@@ -16,36 +10,17 @@ var bot = new Discord.Client({
 });
 
 
+
 bot.on('ready', function (evt) {
     console.log('Connected');
-    console.log('Logged in as: ');
-    console.log(bot.username + ' - (' + bot.id + ')');
-    /*
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-    */
+    console.log(`Logged in as: ${bot.user.tag}`);
 });
 
-/*
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
+bot.on('message', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('Pong!');
+    }
 });
-*/
+
+
+bot.login(auth.token);
